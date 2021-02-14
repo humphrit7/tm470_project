39b24519adf6d401e8d4b1a285f96c2ec209fd99
Thomas Humphries <humphrit7@gmail.com>
2021-02-14 17:54:46 +0000

    Initial commit

---
+++ test_read_gpx.py
@@ -0,0 +1,12 @@
+import unittest
+import read_gpx as gpx
+import pandas as pd
+
+
+class MyTestCase(unittest.TestCase):
+    def test_read_gpx(self):
+        file = 
+
+
+if __name__ == '__main__':
+    unittest.main()
